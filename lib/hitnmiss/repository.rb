8012dcf7538dde17b88ea6c659c6d33f2ab117e2
Andrew De Ponte <cyphactor@gmail.com>
2016-03-16 01:27:50 -0700

    Move Fetcher include up in include order
    Why you made the change:
    
    I did this because things specific to the given repository might depend on
    things Fetcher provides in the future. It seems like a more correct order.

--- lib/hitnmiss/repository.rb
+++ lib/hitnmiss/repository.rb
@@ -9,9 +9,9 @@ module Hitnmiss
     def self.included(mod)
       mod.extend(DriverManagement)
       mod.include(KeyGeneration)
+      mod.include(Fetcher)
       mod.extend(ClassMethods)
       mod.include(InstanceMethods)
-      mod.include(Fetcher)
     end
 
     module ClassMethods
