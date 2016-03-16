147cc2287e2ae77f1bd4b4609f087e2b9df446ee
Andrew De Ponte <cyphactor@gmail.com>
2016-03-16 13:46:25 -0700

    Add fingerprint to Hitnmiss::Entity
    Why you made the change:
    
    I did this so that repository fetchers would be able to pass in externally
    managed fingerprint for the item that is going to be cached by Hitnmiss. This is
    especially useful if you are using the HTTP ETag header on the remote source for
    the item that is going be cached. Primarily, because it would then allow the
    fingerprint to used later on to identify if a cache is stale or not.

--- lib/hitnmiss/entity.rb
+++ lib/hitnmiss/entity.rb
@@ -1,10 +1,11 @@
 module Hitnmiss
   class Entity
-    attr_reader :value, :expiration
+    attr_reader :value, :expiration, :fingerprint
 
-    def initialize(value, expiration_in_seconds=nil)
+    def initialize(value, expiration_in_seconds=nil, fingerprint=nil)
       @value = value
       @expiration = expiration_in_seconds
+      @fingerprint = fingerprint
     end
   end
 end
--- spec/hitnmiss/entity_spec.rb
+++ spec/hitnmiss/entity_spec.rb
@@ -43,4 +43,35 @@ describe Hitnmiss::Entity do
       expect(entity.expiration).to eq(exp_in_secs)
     end
   end
+
+  describe '#fingerprint' do
+    context 'when constructed with a fingerprint' do
+      context 'when constructed with an expiration' do
+        it 'returns the fingerprint' do
+          value = double('value')
+          expiration = double('expiration')
+          fingerprint = double('fingerprint')
+          entity = Hitnmiss::Entity.new(value, expiration, fingerprint)
+          expect(entity.fingerprint).to eq(fingerprint)
+        end
+      end
+
+      context 'when constructed without an expiration' do
+        it 'returns the fingerprint' do
+          value = double('value')
+          fingerprint = double('fingerprint')
+          entity = Hitnmiss::Entity.new(value, nil, fingerprint)
+          expect(entity.fingerprint).to eq(fingerprint)
+        end
+      end
+    end
+
+    context 'when constructed without a fingerprint' do
+      it 'returns nil' do
+        value = double('value')
+        entity = Hitnmiss::Entity.new(value)
+        expect(entity.fingerprint).to be_nil
+      end
+    end
+  end
 end
